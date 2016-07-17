ab3f69c75e0d3f434996414285fbd0c7ee300be1
Wolfman5 <DylanPerry5@gmail.com>
2016-07-17 17:39:11 -0400

    Added information from EmberCookbookJs

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about', {path: '/aboutMe'});
+  this.route('info', function(){
+    this.route('location');
+    this.route('job');
+  });
+  this.route('page-not-found', {path: '/*wildcard' });
 });
 
 export default Router;
